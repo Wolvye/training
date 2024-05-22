1f265e20cd5ec35c6ade9451d0cbbd93ac9b8dfb
Wolvye <alainsoltau@gmail.com>
2024-05-22 14:35:46 +0200

    Update1
    follow the guides

---
+++ script.js
@@ -0,0 +1,47 @@
+let questions = [
+    {   
+        "questiion": "Wie ist der Ruf der Night Lords?",
+        "answer1": "Ave Dominatus Nox",
+        "answer2": "Ave Dominus Nox",
+        "answer3": "Ave Dominion Nox",
+        "answer4": "Ave Dominatus Nocturn",
+        "right_answer": 2
+
+    },
+    {   
+        "questiion": "Wie ist der Ruf der Iron Warrios?",
+        "answer1": "Eisern im Innern, eisern nach außen!",
+        "answer2": "Eisen nach außen, eisern nach innen!",
+        "answer3": "Eisern im Geiste. Eisern im Köper!",
+        "answer4": "Im Geiste eisern, im Körper eisern!",
+        "right_answer": 1
+
+    },
+    {   
+        "questiion": "Wie ist der Ruf der Iron Hands?",
+        "answer1": "Eisern im Innern, eisern nach außen!",
+        "answer2": "Eisen nach außen, eisern nach innen!",
+        "answer3": "Eisern im Geiste. Eisern im Köper!",
+        "answer4": "Im Geiste eisern, im Körper eisern!",
+        "right_answer": 3
+
+    },
+    {   
+        "questiion": "Wie ist der Ruf der Salamanders?",
+        "answer1": "Auf den Amboss des Krieges! Für den Imperator!",
+        "answer2": "Auf den Amboss des Krieges!Ins Feuer der Schlacht!",
+        "answer3": "Ins Feuer der Schlacht! Auf den Amboss der Apotheose!",
+        "answer4": "Ins Feuer der Schlacht! Auf den Amboss des Krieges!",
+        "right_answer": 4
+
+    },
+    {   
+        "questiion": "Wie ist der Ruf der Chorcaradons?",
+        "answer1": "Burn, Maim, Kill!",
+        "answer2": "First in, last out",
+        "answer3": "We have come for you!",
+        "answer4": "Blood for the Bloodgod!",
+        "right_answer": 2
+
+    }
+];
