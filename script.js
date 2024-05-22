feb75a28f37908a37851cc165fa7f624b4d0f876
Wolvye <alainsoltau@gmail.com>
2024-05-22 19:07:32 +0200

    update
    Design, Buttons, questions and answers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
     {   
-        "questiion": "Wie ist der Ruf der Night Lords?",
+        "question": "Wie lautet der Ruf der Night Lords?",
         "answer1": "Ave Dominatus Nox",
         "answer2": "Ave Dominus Nox",
         "answer3": "Ave Dominion Nox",
@@ -9,7 +9,7 @@ let questions = [
 
     },
     {   
-        "questiion": "Wie ist der Ruf der Iron Warrios?",
+        "question": "Wie lautet der Ruf der Iron Warrios?",
         "answer1": "Eisern im Innern, eisern nach außen!",
         "answer2": "Eisen nach außen, eisern nach innen!",
         "answer3": "Eisern im Geiste. Eisern im Köper!",
@@ -18,7 +18,7 @@ let questions = [
 
     },
     {   
-        "questiion": "Wie ist der Ruf der Iron Hands?",
+        "question": "Wie lautet der Ruf der Iron Hands?",
         "answer1": "Eisern im Innern, eisern nach außen!",
         "answer2": "Eisen nach außen, eisern nach innen!",
         "answer3": "Eisern im Geiste. Eisern im Köper!",
@@ -27,7 +27,7 @@ let questions = [
 
     },
     {   
-        "questiion": "Wie ist der Ruf der Salamanders?",
+        "question": "Wie lautet der Ruf der Salamanders?",
         "answer1": "Auf den Amboss des Krieges! Für den Imperator!",
         "answer2": "Auf den Amboss des Krieges!Ins Feuer der Schlacht!",
         "answer3": "Ins Feuer der Schlacht! Auf den Amboss der Apotheose!",
@@ -36,7 +36,7 @@ let questions = [
 
     },
     {   
-        "questiion": "Wie ist der Ruf der Chorcaradons?",
+        "question": "Wie lautet der Ruf der Chorcaradons?",
         "answer1": "Burn, Maim, Kill!",
         "answer2": "First in, last out",
         "answer3": "We have come for you!",
@@ -45,3 +45,23 @@ let questions = [
 
     }
 ];
+
+let currentQuestion=0;
+
+function init(){
+    document.getElementById('allQuestions').innerHTML = questions.length;
+    showQuestion()
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer1').innerHTML = question['answer1'];
+    document.getElementById('answer2').innerHTML = question['answer2'];
+    document.getElementById('answer3').innerHTML = question['answer3'];
+    document.getElementById('answer4').innerHTML = question['answer4'];
+}
+
+function answer(selection){
+    
+}
