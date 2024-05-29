d8a8877ee4ce78df636cb2d522bc6220ea573b56
Wolvye <alainsoltau@gmail.com>
2024-05-29 10:05:20 +0200

    POKEDEX
    Start with new Project

--- DatabaseTraining/remoteStorage.js
+++ DatabaseTraining/remoteStorage.js
@@ -1,16 +1,18 @@
 function onloadFunc(){
     console.log("test");
-    loadData("", {"banana": "rama"});
+    putData("/name",{"banana": "rama"});
 }
 
 const BASE_URL ="https://remotestorage-9593e-default-rtdb.europe-west1.firebasedatabase.app/" ;
 
+
+// Dinge LADEN
 async function loadData(path=""){
     let response= await fetch(BASE_URL+ path + ".json");
     return responseToJson = await response.json();
 }
 
-
+// Dinge POSTEN
 async function postData(path="", data={}){
     let response= await fetch(BASE_URL+ path + ".json",{
         method:"POST",
@@ -20,4 +22,25 @@ async function postData(path="", data={}){
         body: JSON.stringify(data)                      //wichtig ist es zu stringify! 
     });
     return responseToJson = await response.json();
-}
+};
+
+//Dinge LÖSCHEN
+async function deleteData(path=""){
+    let response= await fetch(BASE_URL+ path + ".json",{
+        method:"DELETE",
+    });
+    return responseToJson = await response.json();
+};
+
+//Dinge Put (ändern)
+
+async function putData(path="", data={}){
+    let response= await fetch(BASE_URL+ path + ".json",{
+        method:"PUT",
+        header:{
+            "Content-Type": "application/json",  //achte auf die Syntax!
+        },
+        body: JSON.stringify(data)         
+    });
+    return responseToJson = await response.json();
+};
