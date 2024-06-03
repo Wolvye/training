a7b4b586cd53cbf910910452f9a0ed4efb16b275
Wolvye <alainsoltau@gmail.com>
2024-06-03 22:09:40 +0200

    Update
    new img, new desings, try and error with cards

--- PKMDex/script.js
+++ PKMDex/script.js
@@ -1,26 +1,26 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=14offset=0";
 let responseToJson;
 let allPKM = [];
-const pokemonTypes = [
-    { type: "Normal", imagePath: "./img/normal.png" },
-    { type: "Fire", imagePath: "./img/fire.png" },
-    { type: "Water", imagePath: "./img/water.png" },
-    { type: "Electric", imagePath: "./img/electric.png" },
-    { type: "Grass", imagePath: "./img/grass.png" },
-    { type: "Ice", imagePath: "./img/ice.png" },
-    { type: "Fighting", imagePath: "./img/fighting.png" },
-    { type: "Poison", imagePath: "./img/poison.png" },
-    { type: "Ground", imagePath: "./img/ground.png" },
-    { type: "Flying", imagePath: "./img/flying.png" },
-    { type: "Psychic", imagePath: "./img/psychic.png" },
-    { type: "Bug", imagePath: "./img/bug.png" },
-    { type: "Rock", imagePath: "./img/rock.png" },
-    { type: "Ghost", imagePath: "./img/ghost.png" },
-    { type: "Dragon", imagePath: "./img/dragon.png" },
-    { type: "Dark", imagePath: "./img/dark.png" },
-    { type: "Steel", imagePath: "./img/steel.png" },
-    { type: "Fairy", imagePath: "./img/fairy.png" }
-];
+// const pokemonTypes = [
+//     { type: "Normal", imagePath: "./img/normal.png" },
+//     { type: "Fire", imagePath: "./img/fire.png" },
+//     { type: "Water", imagePath: "./img/water.png" },
+//     { type: "Electric", imagePath: "./img/electric.png" },
+//     { type: "Grass", imagePath: "./img/grass.png" },
+//     { type: "Ice", imagePath: "./img/ice.png" },
+//     { type: "Fighting", imagePath: "./img/fighting.png" },
+//     { type: "Poison", imagePath: "./img/poison.png" },
+//     { type: "Ground", imagePath: "./img/ground.png" },
+//     { type: "Flying", imagePath: "./img/flying.png" },
+//     { type: "Psychic", imagePath: "./img/psychic.png" },
+//     { type: "Bug", imagePath: "./img/bug.png" },
+//     { type: "Rock", imagePath: "./img/rock.png" },
+//     { type: "Ghost", imagePath: "./img/ghost.png" },
+//     { type: "Dragon", imagePath: "./img/dragon.png" },
+//     { type: "Dark", imagePath: "./img/dark.png" },
+//     { type: "Steel", imagePath: "./img/steel.png" },
+//     { type: "Fairy", imagePath: "./img/fairy.png" }
+// ];
 
 async function init() {
     await loadPKM();
@@ -56,12 +56,16 @@ function render() {
     content.innerHTML = "";
     for (let i = 0; i < allPKM.length; i++) {
         let pkm = allPKM[i];
+        let pkmNumber = i + 1;
+
         let type2 = pkm.types[1] ? `
+        
         <div class="MonsterType2">${pkm.types[1].type.name}</div>` : '';  // Überprüfen ob ein zweiter Typ existiert
 
         content.innerHTML += `
             <div class="card" id="cardID" onclick="openPKM(${i})">
-                <h5 class="MonsterName card-title">${pkm.name}</h5>
+                <h5 class="MonsterName card-title">${pkm.name} #${pkmNumber}</h5>
+                
                 <div class="MonsterType1">${pkm.types[0].type.name}</div>
                 ${type2} 
                 <img src="${pkm.sprites.other.dream_world.front_default}" class="MonsterPic">
@@ -75,10 +79,10 @@ function addTypeColor() {
     for (let i = 0; i < allPKM.length; i++) {
         let checkType = allPKM[i].types[0].type.name;
         let card = document.getElementsByClassName('card')[i];
-        
+
         if (typeof checkType === 'string' && checkType.toLowerCase() === 'grass') {
-            card.classList.add("TypeColor-Grass"); 
-            
+            card.classList.add("TypeColor-Grass");
+
         } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'bug') {
             card.classList.add("TypeColor-Bug");
         } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'dark') {
@@ -117,54 +121,52 @@ function addTypeColor() {
     }
 }
 
-function addTypeColor2() {
-    for (let i = 0; i < allPKM.length; i++) {
-        let checkType = allPKM[i].types[0].type.name;
-        let card2 = document.getElementsByClassName('dialog')[i];
-        
-        if (typeof checkType === 'string' && checkType.toLowerCase() === 'grass') {
-            card2.classList.add("TypeColor-Grass"); 
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'bug') {
-            card2.classList.add("TypeColor-Bug");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'dark') {
-            card2.classList.add("TypeColor-Dark");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'dragon') {
-            card2.classList.add("TypeColor-Dragon");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'electric') {
-            card2.classList.add("TypeColor-Electric");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fairy') {
-            card2.classList.add("TypeColor-Fairy");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fighting') {
-            card2.classList.add("TypeColor-Fighting");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fire') {
-            card2.classList.add("TypeColor-Fire");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fly') {
-            card2.classList.add("TypeColor-Fly");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ghost') {
-            card2.classList.add("TypeColor-Ghost");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ground') {
-            card2.classList.add("TypeColor-Ground");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ice') {
-            card2.classList.add("TypeColor-Ice");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'normal') {
-            card2.classList.add("TypeColor-Normal");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'poison') {
-            card2.classList.add("TypeColor-Poison");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'psychic') {
-            card2.classList.add("TypeColor-Psychic");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'rock') {
-            card2.classList.add("TypeColor-Rock");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'steel') {
-            card2.classList.add("TypeColor-Steel");
-        } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'water') {
-            card2.classList.add("TypeColor-Water");
-        }
+function addTypeColor2(index) {
+    let checkType = allPKM[index].types[0].type.name;
+    let card2 = document.getElementsByClassName('dialog')[0];
+    if (typeof checkType === 'string' && checkType.toLowerCase() === 'grass') {
+        card2.classList.add("TypeColor-Grass");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'bug') {
+        card2.classList.add("TypeColor-Bug");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'dark') {
+        card2.classList.add("TypeColor-Dark");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'dragon') {
+        card2.classList.add("TypeColor-Dragon");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'electric') {
+        card2.classList.add("TypeColor-Electric");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fairy') {
+        card2.classList.add("TypeColor-Fairy");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fighting') {
+        card2.classList.add("TypeColor-Fighting");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fire') {
+        card2.classList.add("TypeColor-Fire");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'fly') {
+        card2.classList.add("TypeColor-Fly");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ghost') {
+        card2.classList.add("TypeColor-Ghost");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ground') {
+        card2.classList.add("TypeColor-Ground");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'ice') {
+        card2.classList.add("TypeColor-Ice");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'normal') {
+        card2.classList.add("TypeColor-Normal");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'poison') {
+        card2.classList.add("TypeColor-Poison");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'psychic') {
+        card2.classList.add("TypeColor-Psychic");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'rock') {
+        card2.classList.add("TypeColor-Rock");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'steel') {
+        card2.classList.add("TypeColor-Steel");
+    } else if (typeof checkType === 'string' && checkType.toLowerCase() === 'water') {
+        card2.classList.add("TypeColor-Water");
     }
 }
+
 function openPKM(index) {
     document.getElementById('dialog-bgID').classList.remove('d-none');
     bigCard(index);
-    addTypeColor2();
+    addTypeColor2(index);
 }
 
 function closePKM() {
@@ -175,17 +177,45 @@ function bigCard(index) {
     let bigCard = document.getElementById("dialog-bgID");
     bigCard.innerHTML = "";
     let pkm = allPKM[index];
+    index++;
     let type2 = pkm.types[1] ? `
-    <div class="MonsterType2">${pkm.types[1].type.name}</div>` : ''; 
+    <div class="MonsterType2">${pkm.types[1].type.name}</div>` : '';
 
     bigCard.innerHTML += `
-        <div class="dialog" >
-            <h1 class="bigMonsterName">${pkm.name}</h1>
-            <div class="MonsterType1">${pkm.types[0].type.name}</div>
-                ${type2} 
-            <img src="${pkm.sprites.other.home.front_default}" class="MonsterPic">
+        <div class="dialog " >
+      
+            <div class="headline">
+                <h1 class="bigMonsterName MonsterName"> ${pkm.name}</h1> <div class=hp">${pkm.stats[0].stat.name} ${pkm.stats[0].base_stat} </div>
+                </div>
+                <div class="MonsterType1">${pkm.types[0].type.name}</div>
+                    ${type2} 
+                    <img src="${pkm.sprites.other.dream_world.front_default}" class="MonsterPic">
+
+                    <div class="bottomCard">
+                    <div class="MetaDaten" >#${index} Weight: ${pkm.weight}lb Height: ${pkm.height*10}cm</div>
+                   
+                        <div class="stats">
+                                <h2>Statuswerte</h2>
+                                <div class="hp ">${pkm.stats[0].stat.name} ${pkm.stats[0].base_stat} <div>
+                                <div class="atk">${pkm.stats[1].stat.name} ${pkm.stats[1].base_stat} <div>
+                                <div class="def">${pkm.stats[2].stat.name} ${pkm.stats[2].base_stat} <div>
+                                <div class="spAtk">${pkm.stats[3].stat.name} ${pkm.stats[3].base_stat} <div>
+                                <div class="spDef">${pkm.stats[4].stat.name} ${pkm.stats[4].base_stat} <div>
+                                <div class="speed">${pkm.stats[5].stat.name} ${pkm.stats[5].base_stat} <div>
+                        </div>
+                 
+                        
+                        
            
-            <h2>Statsüberschrift</h2>
+                </div>
+          
+               
+           
+            </div>
         </div>
     `;
 }
+
+
+
+
