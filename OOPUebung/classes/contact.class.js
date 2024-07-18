69e9787d65a0507a6b404c0896573b52d4f27a5e
Wolvye <alainsoltau@gmail.com>
2024-07-18 13:49:26 +0200

    update

---
+++ OOPUebung/classes/contact.class.js
@@ -0,0 +1,16 @@
+class Contact{
+    firstname;
+    lastName;
+
+    constructor(fN,lN){
+        this.fN=fN;
+        this.lN=lN;
+    }
+}
+// Beschreibung, wie ein Objekt aussehen soll, Vorlage, Schablone
+
+// class Contact() 
+//    {
+//     firstname; undefined
+//     lastName; undefined
+// } Das Class Contact erzeugt ein neues JSON Array 
