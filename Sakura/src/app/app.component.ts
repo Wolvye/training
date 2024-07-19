aaba1e94d40b21ee6920e79e40f9d83655541c02
Wolvye <alainsoltau@gmail.com>
2024-07-19 12:02:44 +0200

    Update Fonts

--- Sakura/src/app/app.component.ts
+++ Sakura/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet,LandingPageComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
