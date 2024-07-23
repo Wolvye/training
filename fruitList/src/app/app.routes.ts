1529b0c35b14b00fb39e7422e7807ecc46bc13cd
Wolvye <alainsoltau@gmail.com>
2024-07-23 10:12:06 +0200

    Update Training

---
+++ fruitList/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
+
+export const routes: Routes = [
+    { path: '', component: FruitlistComponent },
+
+
+];
