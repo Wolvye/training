3d0823c415571ae7a22f98cb19c3e2ce536c1b1b
Wolvye <alainsoltau@gmail.com>
2024-07-30 16:47:10 +0200

    update

--- InstaAngularV17/src/app/app.routes.ts
+++ InstaAngularV17/src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main/main.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path: '', component: MainComponent},
+     {path: 'contact', component: ContactComponent},
+];
--- InstaAngularV17/src/app/main/main/main.component.ts
+++ InstaAngularV17/src/app/main/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
@@ -12,19 +13,30 @@ export class MainComponent {
     Name: "Hansi",
     imgPath: "./assets/img/banana.jpg",
     likeCounter: 123,
-
+    isliked: false,
   },
   {
     Name: "Petri",
     imgPath: "./assets/img/currant.jpg",
     likeCounter: 555,
+    isliked: false,
   },
   {
     Name: "Dieter",
-    imgPath: "./assets/img/heart-black.png",
+    imgPath: "./assets/img/orange.jpg",
     likeCounter: 666,
+    isliked: true,
   },
 
   ];
+  toggleLiked(index: number) {
+    if (this.posts[index].isliked) {
+      this.posts[index].isliked = false;
+      this.posts[index].likeCounter-=1;
 
+    } else {
+      this.posts[index].isliked = true;
+      this.posts[index].likeCounter+=1;
+    }
+  }
 }
